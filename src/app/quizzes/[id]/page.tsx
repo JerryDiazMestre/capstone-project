e90a73cbeb3407d21fa4b7bc69a035e3fa2d8f28
JerryDiazMestre <gerardo.diaz.mestre@gmail.com>
2023-08-17 19:48:15 -0400

    T748

--- src/app/quizzes/[id]/page.tsx
+++ src/app/quizzes/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import 'bootstrap/dist/css/bootstrap.css';
 import React, {useState} from 'react'
-import {getQuestion, getQuiz} from '../../api/fetcher';
+import {getQuiz} from '../../api/fetcher';
 import useSWR from "swr";
 import { Quiz } from '@/app/model/Quiz';
 
@@ -11,21 +11,25 @@ type Props = {
 
 
 export default function page({params}: Props) {
-    let questions = [];
+    // let questions = [];
+    // const [questions, setQuestions] = useState<any>([]);
     const { data, error, isLoading } = useSWR(`quizzes/${params.id}`, getQuiz);
     let quiz = data;
 
     console.log({quiz:quiz, questions:quiz?.questionIds});
-    if (quiz && quiz?.questionIds && quiz?.questionIds?.length){
-        quiz?.questionIds.map((item:string, index:number) =>{
-            const { data, error } = useSWR(`${item}`, getQuestion);
-            questions.push(data);
-        });
+
+    if (quiz && quiz?.questionIds && quiz?.questionIds?.length > 0){
+        // quiz?.questionIds.map((item:string, index:number) =>{
+        //     const { data, error } = useSWR(`${item}`, getQuestion);
+        //     setQuestions([...questions, data]);
+            // questions.push(data);
+        // });
     }
+
     return (
         <>
             <h1>{quiz?.title}</h1>   
-            <h5>{quiz?.questionIds}</h5>
+            {/* <Question /> */}
         </>
     )
 }
