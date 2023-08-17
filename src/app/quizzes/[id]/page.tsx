bb021ea9462afac2569ab0faed0bb36b294c7ca2
JerryDiazMestre <gerardo.diaz.mestre@gmail.com>
2023-08-17 19:51:29 -0400

    T751

--- src/app/quizzes/[id]/page.tsx
+++ src/app/quizzes/[id]/page.tsx
@@ -13,22 +13,23 @@ type Props = {
 export default function page({params}: Props) {
     // let questions = [];
     // const [questions, setQuestions] = useState<any>([]);
-    const { data, error, isLoading } = useSWR(`quizzes/${params.id}`, getQuiz);
-    let quiz = data;
+    // const { data, error, isLoading } = useSWR(`quizzes/${params.id}`, getQuiz);
+    // let quiz = data;
 
-    console.log({quiz:quiz, questions:quiz?.questionIds});
+    // console.log({quiz:quiz, questions:quiz?.questionIds});
 
-    if (quiz && quiz?.questionIds && quiz?.questionIds?.length > 0){
+    // if (quiz && quiz?.questionIds && quiz?.questionIds?.length > 0){
         // quiz?.questionIds.map((item:string, index:number) =>{
         //     const { data, error } = useSWR(`${item}`, getQuestion);
         //     setQuestions([...questions, data]);
             // questions.push(data);
         // });
-    }
+    // }
 
     return (
         <>
-            <h1>{quiz?.title}</h1>   
+            Help me!!!
+            {/* <h1>{quiz?.title}</h1>    */}
             {/* <Question /> */}
         </>
     )
